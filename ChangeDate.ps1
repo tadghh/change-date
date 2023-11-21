ee2d4f75092dbf24aafb02c8dda2c218a0d5e039
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-11-20 20:27:20 -0600

    Fix date time error
    incorrectly got the current hour

--- ChangeDate.ps1
+++ ChangeDate.ps1
@@ -25,8 +25,9 @@ param(
 $file = Get-Item ($filePath -replace "'", "")
 $file.LastWriteTime = Get-Date
 
-# Add a random time between 12:00 PM and 5:00 PM, also making sure its not in the future
-$maxRandomHour = [math]::Min(17, $currentDate.Hour)
+# Add a random time between 12:00 PM and 5, also making sure its not in the future
+$maxRandomHour = [math]::Min(17, (Get-Date).Hour)
+
 $randomTime = Get-Random -Minimum 12 -Maximum $maxRandomHour
 
 $file.LastWriteTime = $file.LastWriteTime.Date.AddHours($randomTime).AddMinutes((Get-Random -Minimum 0 -Maximum 57)).AddSeconds((Get-Random -Minimum 0 -Maximum 59))
